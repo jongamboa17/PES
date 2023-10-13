801ee66dfc253d02215dab7747616cbfb1381119
Jonathan Gamboa <jongamboa@gmail.com>
2023-10-13 00:36:26 -0600

    cambio en navbar

--- components/home_admin.js
+++ components/home_admin.js
@@ -56,7 +56,7 @@ export default function Home_admin() {
               </div>
               
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <p className='text-stone-50'>Roberto Muñoz</p>
+                <p className='text-stone-50'>Roberto Saenz</p>
                 {/* Profile dropdown */}
                 <Menu as="div" className="relative ml-3">
                   <div>

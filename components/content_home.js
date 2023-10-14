315c6b06b71f038997ffc9d6041876abc95a0509
Jonathan Gamboa <jongamboa@gmail.com>
2023-10-14 02:16:41 -0600

    agregar tabla de alumnos por asignar

--- components/content_home.js
+++ components/content_home.js
@@ -30,12 +30,63 @@ export default function Content_home (){
             </div> 
             {/*div del centro*/}
             <div className="py-5 px-5 grid grid-cols-2 gap-x-2 gap-y-3 grid-flow-row-dense ">
-                <div className="bg-slate-100 rounded-lg shadow-xl min-h-[50px] row-span-5">                    
-                    
-
-    
+                <div className="bg-slate-100 rounded-lg shadow-xl min-h-[400px] row-span-5">                    
+                    {/*tabla alumnos activos no asignados*/}
+                    <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+                    <center><h2 className="font-bold py-3">Alumnos Por Asignar</h2></center>
+                        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                
+                                <tr>
+                                    <th scope="col" className="p-4">
+                                        <div className="flex items-center">
+                                            
+                                            <label for="checkbox-all-search" className="sr-only">checkbox</label>
+                                        </div>
+                                    </th>
+                                    <th scope="col" className="px-6 py-3">
+                                        Nombre
+                                    </th>
+                                    <th scope="col" className="px-6 py-3">
+                                        Contacto
+                                    </th>
+                                    
+                                    <th scope="col" className="px-6 py-3">
+                                        
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                    <td className="w-4 p-4">
+                                        <div className="flex items-center">
+                                            
+                                            <label for="checkbox-table-search-1" className="sr-only">checkbox</label>
+                                        </div>
+                                    </td>
+                                    <th scope="row" className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
+                                        <Image fill={true} className="w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-1.jpg" alt="Jese image "></Image>
+                                        <div className="pl-3">
+                                            <div className="text-base font-semibold">Neil Sims</div>
+                                            <div className="font-normal text-gray-500">neil.sims@flowbite.com</div>
+                                        </div>  
+                                    </th>
+                                    <td className="px-6 py-4">
+                                        88888888
+                                    </td>
+                                    
+                                    <td className="px-6 py-4">
+                                        <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Asignar Alumno</a>
+                                    </td>
+                                </tr>
+                                
+                            </tbody>
+                        </table>
+                    </div>
                 </div> 
-                <div className="bg-slate-100 rounded-lg shadow-xl min-h-[50px] row-span-5"></div>
+                <div className="bg-slate-100 rounded-lg shadow-xl min-h-[400px] row-span-5">
+                    
+                </div>
             </div>
             {/*div de las estadisticas de abajo*/}
             <div class="sm:hidden">
@@ -49,15 +100,15 @@ export default function Content_home (){
                     </div>
                     <div class="bg-blue-700 rounded-md flex flex-col items-center justify-center py-2">
                         <dt class="mb-2 text-3xl text-white font-extrabold ">73</dt>
-                        <dd class="text-white dark:text-gray-400">Usuarios Activos</dd>
+                        <dd class="text-white dark:text-gray-400">Usuarios Inactivos</dd>
                     </div>
                     <div class="bg-blue-700 rounded-md flex flex-col items-center justify-center py-2">
                         <dt class="mb-2 text-3xl text-white font-extrabold ">73</dt>
-                        <dd class="text-white dark:text-gray-400">Usuarios Activos</dd>
+                        <dd class="text-white dark:text-gray-400">Docentes</dd>
                     </div>
                     <div class="bg-blue-700 rounded-md flex flex-col items-center justify-center py-2">
                         <dt class="mb-2 text-3xl text-white font-extrabold ">73</dt>
-                        <dd class="text-white dark:text-gray-400">Usuarios Activos</dd>
+                        <dd class="text-white dark:text-gray-400">Asignaturas</dd>
                     </div>
             </div>
         </div>
